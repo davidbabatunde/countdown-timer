a5c619fc3de144d103997f4e9393afdbacc68b6d
Reol <reol0624@gmail.com>
2022-08-30 16:40:02 +0100

    most working, next to add flip and hover states

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Timer from "./Timer";
 
 function App() {
   return (
-    <div className="app">
-      <div className="title">
-        <h2>WE'RE LAUNCHING SOON!</h2>
+    <div id="app">
+      <div id="title">
+        <h2>MY MUM'S BIRTHDAY SOON!</h2>
       </div>
       <Timer />
       <Footer />
